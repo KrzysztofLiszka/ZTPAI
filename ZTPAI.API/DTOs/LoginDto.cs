1d58f75bf164066867e0659b4b9dd9370d09cec8
Krzysztof Liszka <krzysztof.liszkaa@wp.pl>
2023-04-29 17:28:13 +0200

    Add: user identity + jwt token

---
+++ ZTPAI.API/DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ZTPAI.API.DTOs
+{
+    public class LoginDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ ZTPAI.API/Migrations/20230428150012_Add-Users-Table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZTPAI.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
